419fb0bc04240d08f6e8dc4cb7689d95c9763468
EudesFreitas <eudes.fcine@gmail.com>
2026-03-22 22:52:03 -0300

    pagina atualizada

--- javascript/script.js
+++ javascript/script.js
@@ -64,3 +64,12 @@ button2.addEventListener("click", () => {
   }
 
 });
+
+
+// para abrir o menu
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
